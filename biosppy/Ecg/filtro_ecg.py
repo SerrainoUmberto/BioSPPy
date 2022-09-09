f332d3ecac459d7f32054e49248d8793d8b1ebb4
SerrainoUmberto <109298250+SerrainoUmberto@users.noreply.github.com>
2022-09-09 15:26:35 +0200

    Delete biosppy directory

--- biosppy/Ecg/filtro_ecg.py
+++
@@ -1,6 +0,0 @@
-from biosppy.signals import ecg
-def filtro_ecg(segnale):
-    out=ecg.ecg(signal=segnale,sampling_rate=500, show=False)
-    return out[1]
-    
-    
--- biosppy/Ecg/plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-def plot_ecg(segnale,tempo):
-    plt.figure(figsize=(10,8)) #definisco la misura della pagina che presenta il grafico
-    plt.plot(tempo,segnale)
-    plt.grid() #decido che il grafico contenga righe e colonne
-    plt.title('ECG') #imposto titolo
-    plt.xlabel('Tempo (Secondi)') #imposto nome delle ascisse
-    plt.ylabel('d.d.p(Millivolt)') #imposto nome delle ordinate
-    plt.show()
